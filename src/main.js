ebbb66c826d6a69d0a93215c4da4de1fb7e8677a
Yanislav Pavlov <yanislav.pavlov@kyto.com>
2021-04-24 14:05:01 +0200

    Add font awesome free

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import '@fortawesome/fontawesome-free/css/all.css'
 
 createApp(App)
     .use(router)
