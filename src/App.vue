d4c3faf51e6de792f0322a17a603c90ae6944a47
Yanislav Pavlov <yanislav.pavlov@kyto.com>
2021-04-24 20:35:56 +0200

    Create footer

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,96 @@
 <template>
     <Header/>
     <router-view/>
+    <Footer/>
 </template>
 
 <script>
 import Header from './components/basic/Header';
+import Footer from './components/basic/Footer';
 
 export default {
     components: {
-        Header
+        Header,
+        Footer
     }
 }
 </script>
 <style>
+@font-face {
+    font-family: Repo;
+    src: url("assets/fonts/Repo-Light.eot?#iefix") format("embedded-opentype"),
+    url("assets/fonts/Repo-Light.otf") format("opentype"),
+    url("assets/fonts/Repo-Light.woff") format("woff"),
+    url("assets/fonts/Repo-Light.ttf") format("truetype"),
+    url("assets/fonts/Repo-Light.svg#Repo-Light") format("svg");
+    font-weight: 300;
+    font-style: normal;
+}
+@font-face {
+    font-family: Repo;
+    src: url("assets/fonts/Repo.eot?#iefix") format("embedded-opentype"),
+    url("assets/fonts/Repo.otf") format("opentype"),
+    url("assets/fonts/Repo.woff") format("woff"),
+    url("assets/fonts/Repo.ttf") format("truetype"),
+    url("assets/fonts/Repo.svg#Repo") format("svg");
+    font-weight: 400;
+    font-style: normal;
+}
+@font-face {
+    font-family: Repo;
+    src: url("assets/fonts/Repo-Medium.eot?#iefix") format("embedded-opentype"),
+    url("assets/fonts/Repo-Medium.otf") format("opentype"),
+    url("assets/fonts/Repo-Medium.woff") format("woff"),
+    url("assets/fonts/Repo-Medium.ttf") format("truetype"),
+    url("assets/fonts/Repo-Medium.svg#Repo-Medium") format("svg");
+    font-weight: 500;
+    font-style: normal;
+}
+@font-face {
+    font-family: Repo-DemiBold;
+    src: url("assets/fonts/Repo-DemiBold.eot?#iefix") format("embedded-opentype"),
+    url("assets/fonts/Repo-DemiBold.otf") format("opentype"),
+    url("assets/fonts/Repo-DemiBold.woff") format("woff"),
+    url("assets/fonts/Repo-DemiBold.ttf") format("truetype"),
+    url("assets/fonts/Repo-DemiBold.svg#Repo-DemiBold") format("svg");
+    font-weight: 600;
+    font-style: normal;
+}
+@font-face {
+    font-family: Repo;
+    src: url("assets/fonts/Repo-Bold.eot?#iefix") format("embedded-opentype"),
+    url("assets/fonts/Repo-Bold.otf") format("opentype"),
+    url("assets/fonts/Repo-Bold.woff") format("woff"),
+    url("assets/fonts/Repo-Bold.ttf") format("truetype"),
+    url("assets/fonts/Repo-Bold.svg#Repo-Bold") format("svg");
+    font-weight: 700;
+    font-style: normal;
+}
+@font-face {
+    font-family: Repo;
+    src: url("assets/fonts/Repo-ExtraBold.eot?#iefix") format("embedded-opentype"),
+    url("assets/fonts/Repo-ExtraBold.otf") format("opentype"),
+    url("assets/fonts/Repo-ExtraBold.woff") format("woff"),
+    url("assets/fonts/Repo-ExtraBold.ttf") format("truetype"),
+    url("assets/fonts/Repo-ExtraBold.svg#Repo-ExtraBold") format("svg");
+    font-weight: 800;
+    font-style: normal;
+}
+@font-face {
+    font-family: Repo;
+    src: url("assets/fonts/Repo-Black.eot?#iefix") format("embedded-opentype"),
+    url("assets/fonts/Repo-Black.otf") format("opentype"),
+    url("assets/fonts/Repo-Black.woff") format("woff"),
+    url("assets/fonts/Repo-Black.ttf") format("truetype"),
+    url("assets/fonts/Repo-Black.svg#Repo-Black") format("svg");
+    font-weight: 900;
+    font-style: normal;
+}
 #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
+    font-family: Repo, sans-serif;
+}
+
+.bg-dark-blue {
+    background-color: #1a3156;
 }
 </style>
---
+++ src/components/basic/Footer.vue
@@ -0,0 +1,45 @@
+<template>
+    <footer class="bg-dark-blue text-white p-3">
+        <div class="row text-center border-bottom pb-3 mb-3">
+            <div class="col-md-3">
+                <a href="#" title="title" target="_blank">title</a>
+            </div>
+        </div>
+        <div class="row text-center small">
+            <div class="col-md-4 small p-3">
+                &copy; {{ copyright() }}.
+            </div>
+            <div class="col-md-4 m-auto small">
+                <a class="pe-2" href="#" rel="nofollow" title="Поверителност"><i class="fas fa-user-shield text-white"></i> Поверителност</a>
+                <a class="ms-2" href="#" rel="nofollow" title="Бисквитки"><i class="fas fa-cookie-bite text-white"></i> Бисквитки</a>
+            </div>
+            <div class="col-md-4 small p-3">
+                {{ webmaster.text }}:
+                <a :href="webmaster.url" :title="webmaster.text" target="_new"><img style="height:20px" :src="require(`@/assets/${webmaster.logo}`)" :alt="webmaster.name + ' лого'"></a>
+            </div>
+        </div>
+    </footer>
+</template>
+
+<script>
+import { websiteInfo, webmaster } from '../../composable/staticData';
+
+export default {
+    data() {
+        return {
+            websiteInfo,
+            webmaster
+        }
+    },
+    methods: {
+        copyright() {
+            const year = new Date().getFullYear();
+            return `${year} ${this.websiteInfo.shortName} "${websiteInfo.name}" - ${websiteInfo.location}`
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
