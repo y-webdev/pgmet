7bc8ad3f1fc801d4e150eb3ae14e44ecefc6bbd0
Yanislav Pavlov <yanislav.pavlov@kyto.com>
2021-04-26 19:46:04 +0200

    Add page title and meta tags

--- src/composable/staticData.js
+++ src/composable/staticData.js
@@ -8,12 +8,27 @@ export const websiteInfo = {
     logo: 'logo_pgmet.png'
 }
 
+const title = `${websiteInfo.shortName} "${websiteInfo.name}" - ${websiteInfo.location}`
+
 export const navItems = [
     {
         path: '/',
         name: 'Начало',
         icon: 'fas fa-home',
-        component: Home
+        component: Home,
+        meta: {
+            title: `Начало | ${title}`,
+            metaTags: [
+                {
+                    name: 'description',
+                    content: 'The home page of our example app.'
+                },
+                {
+                    property: 'og:description',
+                    content: 'The home page of our example app.'
+                }
+            ]
+        }
     },
     {
         path: '/about',
@@ -21,17 +36,56 @@ export const navItems = [
         // route level code-splitting
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "about" */ '@/views/About.vue')
+        component: () => import(/* webpackChunkName: "about" */ '@/views/About.vue'),
+        meta: {
+            title: `За нас | ${title}`,
+            metaTags: [
+                {
+                    name: 'description',
+                    content: 'The home page of our example app.'
+                },
+                {
+                    property: 'og:description',
+                    content: 'The home page of our example app.'
+                }
+            ]
+        }
     },
     {
         path: '/projects',
         name: 'Проекти',
-        component: () => import('@/views/Projects.vue')
+        component: () => import('@/views/Projects.vue'),
+        meta: {
+            title: `Проекти | ${title}`,
+            metaTags: [
+                {
+                    name: 'description',
+                    content: 'The home page of our example app.'
+                },
+                {
+                    property: 'og:description',
+                    content: 'The home page of our example app.'
+                }
+            ]
+        }
     },
     {
         path: '/news',
         name: 'Новини',
-        component: () => import('@/views/News.vue')
+        component: () => import('@/views/News.vue'),
+        meta: {
+            title: `Новини | ${title}`,
+            metaTags: [
+                {
+                    name: 'description',
+                    content: 'The home page of our example app.'
+                },
+                {
+                    property: 'og:description',
+                    content: 'The home page of our example app.'
+                }
+            ]
+        }
     }
 ]
 
@@ -40,20 +94,59 @@ export const footerNavItems = [
         path: '/privacy',
         name: 'Поверителност',
         icon: 'fas fa-user-shield',
-        component: () => import('@/views/Privacy.vue')
+        component: () => import('@/views/Privacy.vue'),
+        meta: {
+            title: `Поверителност | ${title}`,
+            metaTags: [
+                {
+                    name: 'description',
+                    content: 'The home page of our example app.'
+                },
+                {
+                    property: 'og:description',
+                    content: 'The home page of our example app.'
+                }
+            ]
+        }
     },
     {
         path: '/cookies',
         name: 'Бисквитки',
         icon: 'fas fa-cookie-bite',
-        component: () => import('@/views/Cookies.vue')
+        component: () => import('@/views/Cookies.vue'),
+        meta: {
+            title: `Бисквитки | ${title}`,
+            metaTags: [
+                {
+                    name: 'description',
+                    content: 'The home page of our example app.'
+                },
+                {
+                    property: 'og:description',
+                    content: 'The home page of our example app.'
+                }
+            ]
+        }
     },
 ]
 
 export const contact = {
     path: '/contacts',
     name: 'Контакти',
-    component: () => import('@/views/Contacts.vue')
+    component: () => import('@/views/Contacts.vue'),
+    meta: {
+        title: `Контакти | ${title}`,
+        metaTags: [
+            {
+                name: 'description',
+                content: 'The home page of our example app.'
+            },
+            {
+                property: 'og:description',
+                content: 'The home page of our example app.'
+            }
+        ]
+    }
 }
 
 export const webmaster = {
