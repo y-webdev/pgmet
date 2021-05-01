90f65406175b5c7999f2fc6d28d5fc689e4d0a00
Yanislav Pavlov <yanislav.pavlov@kyto.com>
2021-05-01 18:10:08 +0200

    Install lightbox2

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import router from './router'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '@fortawesome/fontawesome-free/css/all.css'
+import 'lightbox2/dist/js/lightbox-plus-jquery.js'
+import 'lightbox2/dist/css/lightbox.css'
 
 createApp(App)
     .use(router)
